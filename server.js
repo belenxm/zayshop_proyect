4808cd17f6c8fdc258b76bc4b742dda584b04277
fer.belen <belenxmorales@gmail.com>
2023-02-03 14:55:43 -0300

    inicio y about pasados a hbs

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require ('express')
 const app = express()
+var hbs = require('hbs');
+
 const port = 3000
+
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static('public'));
 
 
-//app.get ('/', (req, res) => {
-  //  res.send ('Hello world')
+app.get ('/', (req, res) => {
+   res.render('index',{
+    titulo: "mi shop"
+   }) 
+})
 
-//})
+
+app.get ('/about', (req, res) => {
+   res.render('about',{
+    titulo: "Sobre Nosotros"
+   }) 
+})
+
+ 
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+}) 
